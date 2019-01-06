83722a20e776b0126b2bf89abeb7542c9baea554
Andrew Mungall <andrewmungall@gmail.com>
2019-01-06 20:50:23 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  string = string.toUpperCase()
+console.log(string)
+}
+
+function logWhisper(string) {
+  string = string.toLowerCase()
+console.log(string)
+}
+
+var lowercase = "hello";
+var uppercase = "HELLO";
+function sayHiToGrandma(string) {
+If (string = lowercase)
+return "I can't hear you!";
+} else if(string = uppercase) {
+return "YES INDEED!";
+} else (string = "I love you, Grandma.") {
+return "I love you, too.";
+}
+}
